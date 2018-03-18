e75babd4f9d55271137ddfb9f5760fd8a8068a54
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-18 16:50:01 +0530

    added customer service dependancy to myresource Change-Id: Id4680c070cd3beda866ad2daef46c464e6ea286c

--- test/src/main/java/com/samsl/test/config/MyResource.java
+++ test/src/main/java/com/samsl/test/config/MyResource.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.samsl.test.service.CustomerService;
 /**
  * Root resource (exposed at "myresource" path)
  */
