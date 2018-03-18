6d76647864caf6e62002844cf1bb431267071272
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-18 16:07:21 +0530

    added my resource,customer resource
    Change-Id: I4a8302c37fd3b1fa5fdb6e12f778de5c26b4c3fd

---
+++ test/src/main/java/com/samsl/test/config/MyResource.java
@@ -0,0 +1,33 @@
+package com.samsl.test.config;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+	 @Autowired
+	 CustomerService customerService;
+	 
+	 @GET
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String getIt(){
+	 return customerService.getStringTest();
+	 }
+	
+
+	
+	
+
+
+	   
+
+
+	   
+	
+}
