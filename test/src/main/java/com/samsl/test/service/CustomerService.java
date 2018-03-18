70fb0fbd2a26130ddcc2483dabdc3137bb588ab5
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-18 16:24:00 +0530

    Added customer service
    Change-Id: I59101c947a46b0a8c936d9d8fd25cce2b4a1146e

---
+++ test/src/main/java/com/samsl/test/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.samsl.test.service;
+import java.util.List;
+
+public interface CustomerService {
+
+
+	
+
+	import org.pramod.model.Customer;
+
+	public interface CustomerService {
+
+	    String getStringTest();
+	    Customer addCustomer(Customer customer);
+	    Customer updateCustomer(int id, Customer customer);
+	    Customer deleteCustomer(int id);
+	    Customer getCustomerById(int id);
+	    List<Customer> getAllCustomer();
+	}
+
+}
---
+++ test/src/main/java/com/samsl/test/service/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package com.samsl.test.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerServiceImp limplements CustomerService {
+
+	  @Autowired
+	  private CustomerDao customerDao; 
+
+	  @Override
+	  public String getStringTest() {
+	    return customerDao.getStringTest();
+	  }
+
+	  @Override
+	  public Customer addCustomer(Customer customer) {
+	    return customerDao.addCustomer(customer);
+	  }
+
+	  @Override
+	  public Customer updateCustomer(int id, Customer customer) {
+	    return customerDao.updateCustomer(id, customer);
+	  }
+
+	  @Override
+	  public Customer deleteCustomer(int id) {
+	    return customerDao.deleteCustomer(id);
+	  }
+
+	  @Override
+	  public Customer getCustomerById(int id) {
+	    return customerDao.getCustomerById(id);
+	  }
+
+	  @Override
+	  public List<Customer> getAllCustomer() {
+	    return customerDao.getAllCustomer();
+	  }
+	
+}
